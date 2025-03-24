640add6353fcc0f4a6ad93892e66a2a3061ffeb5
AronCodeCraft <aronbestowed711@gmail.com>
2025-03-24 22:26:14 +0530

    Adding the deleted files back

---
+++ file2.py
@@ -0,0 +1,8 @@
+def sum_of_two(m,n):
+    if n<m:
+        print(f"Sum of {m} and {n} : {m+n}")
+    else:
+        print(f"Second Number is larger than first, Sum of {m} and {n} : {m+n}")
+
+          
+sum_of_two(4,5)
